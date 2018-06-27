f2d1f45fdfcf884a1063758ccfc24b500a393c08
Ankita Rath <rathankita8@gmail.com>
2018-06-27 18:37:26 +0530

    project in progress

---
+++ Assignment5/JS/validateRegForm.js
@@ -0,0 +1,10 @@
+
+function enterMoreFields(){
+    var hidden=document.getElementsByClassName("hidden");
+    var show= document.getElementsByClassName("show");
+    hidden.style.display="block";
+    // for(var i=0;i < hidden.length;i++){
+    // hidden[i].style.display="block";
+    // }
+   
+}
